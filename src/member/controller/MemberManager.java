12c347c5eba1a88fd40d667aae449f47b1164888
SSE <SSE@DESKTOP-9E2DCVI>
2020-03-24 14:44:45 +0900

    메소드 updateName추가

--- src/member/controller/MemberManager.java
+++ src/member/controller/MemberManager.java
@@ -32,9 +32,109 @@ public class MemberManager {
 		
 	}
 
+	public void searchId() {
+		
+		System.out.print("검색할 아이디를 입력하세요 : ");
+		String id = sc.next();
+		
+		for(int i = 0; i < m.length; i++) {
+			if(m[i].getUserId().equals(id)) {
+				//printOne();
+			} else {
+				System.out.println("검색한 회원 정보가 존재하지 않습니다.");
+				return;
+			}
+			
+			
+		}
+	}
+	
+	public void searchName() {
+		
+		System.out.print("검색할 이름을 입력하세요 : ");
+		String name = sc.next();
+		
+		for(int i = 0; i < m.length; i++) {
+			if(m[i].getUserName().equals(name)) {
+				//printOne();
+			} else {
+				System.out.println("검색한 회원 정보가 존재하지 않습니다.");
+				return;
+			}
+		}
+		
+	}
+	
+	public void searchEmail() {
+		
+		System.out.print("검색할 이메일을 입력하세요 : ");
+		String email = sc.next();
+		
+		for(int i = 0; i < m.length; i++) {
+			if(m[i].getEmail().equals(email)) {
+				//printOne();
+			} else {
+				System.out.println("검색한 회원 정보가 존재하지 않습니다.");
+				return;
+			}
+		}
+	}
+	
+	public void updatePwd() {
+		
+		System.out.print("수정할 회원의 아이디를 입력하세요 : ");
+		String id = sc.next();
+		
+		for(int i = 0; i < m.length; i++) {
+			if(m[i].getUserId().equals(id)) {
+				System.out.print("변경할 비밀번호를 입력하세요 : ");
+				String pwd = sc.next();
+				m[i].setUserPwd(pwd);
+				System.out.println("패스워드 수정이 완료되었습니다.");
+				return;
+			} else {
+				System.out.println("수정할 회원이 존재하지 않습니다.");
+			}
+		}
+	}
+	
+	public void updateName() {
+		
+		System.out.print("수정할 회원의 아이디를 입력하세요 : ");
+		String id = sc.next();
+		
+		for(int i = 0; i < m.length; i++) {
+			if(m[i].getUserId().equals(id)) {
+				System.out.print("변경할 회원의 이름을 입력하세요 : ");
+				String name = sc.next();
+				m[i].setUserId(id);
+				System.out.println("이름 수정이 완료되었습니다.");
+				return;
+			}
+		}
+	}
+	
+	public void updateEmail() {
+		
+	}
+	
 	public static void printAllMember() {
+		
 		
 	}
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
